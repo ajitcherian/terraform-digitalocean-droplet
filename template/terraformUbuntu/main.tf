75b61fab126847db75324beb02bba437fc3e87d5
ajitcherian <54443253+ajitcherian@users.noreply.github.com>
2021-05-27 17:10:24 +0530

    Update main.tf

--- template/terraformUbuntu/main.tf
+++ template/terraformUbuntu/main.tf
@@ -33,14 +33,14 @@ provisioner "remote-exec" {
 }
 
 resource "digitalocean_firewall" "server" {
-  name = "server-firewall"
+  name = "server-SG"
 
   droplet_ids = [digitalocean_droplet.server.id]
 
   inbound_rule {
     protocol         = "tcp"
     port_range       = "22"
-    source_addresses = ["192.168.1.0/24", "2002:1:2::/48"]
+    source_addresses = ["0.0.0.0/0", "::/0"]
   }
 
   inbound_rule {
@@ -55,25 +55,17 @@ resource "digitalocean_firewall" "server" {
     source_addresses = ["0.0.0.0/0", "::/0"]
   }
 
-  inbound_rule {
-    protocol         = "icmp"
-    source_addresses = ["0.0.0.0/0", "::/0"]
-  }
-
   outbound_rule {
     protocol              = "tcp"
-    port_range            = "53"
+    port_range       = "1-65535"
     destination_addresses = ["0.0.0.0/0", "::/0"]
   }
 
   outbound_rule {
     protocol              = "udp"
-    port_range            = "53"
+    port_range       = "1-65535"
     destination_addresses = ["0.0.0.0/0", "::/0"]
   }
 
-  outbound_rule {
-    protocol              = "icmp"
-    destination_addresses = ["0.0.0.0/0", "::/0"]
-  }
 }
+
