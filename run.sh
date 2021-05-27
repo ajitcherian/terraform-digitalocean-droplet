31efc068319e28611a1a42f506de9b1f0a08b048
ajitcherian <54443253+ajitcherian@users.noreply.github.com>
2021-05-27 12:22:49 +0530

    Create run.sh
    Script to create droplet

---
+++ run.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+#change droplet name in variable server and droplet public in terraform plan
+SERVER=$1
+SERVER_PUBLIC_KEY=$2
+OS=$3
+sudo mkdir -p /tmp/server/$SERVER/terraform
+sudo chmod 777 /tmp/server/$SERVER
+
+
+# this will not stop the script and ask for password IT will create private key with name $SERVER
+ssh-keygen -b 2048 -t rsa -f /tmp/server/$SERVER/$SERVER -q  -N ""
+
+#add key to digital ocean
+file=$(cat /tmp/server/$SERVER/$SERVER_PUBLIC_KEY)
+sudo doctl compute ssh-key create $SERVER --public-key "$file"
+
+#Get fingerprint id
+sudo rm -rf text
+sudo doctl compute ssh-key list | grep $SERVER | rev |  cut -c -47 | rev > /tmp/text
+
+# Copy only the plan and variable files
+sudo cp -r  /tmp/template/terraformUbuntu/*.tf /tmp/server/$SERVER/terraform/
+
+if [[ $OS = "ubuntu" ]]
+then
+  sudo cp -r  ./template/terraformUbuntu/*.tf /tmp/server/$SERVER/terraform/
+else
+  sudo cp -r  ./template/terraformCentos/*.tf /tmp/server/$SERVER/terraform/
+fi
+
+
+
+
+# Renaming the resource
+sudo sed -i 's/server/'$1'/g' /tmp/server/$SERVER/terraform/main.tf
+sudo sed -i 's/s-1vcpu-1gb/'$4'/g' /tmp/server/$SERVER/terraform/main.tf
+sudo sed -i 's/nyc1/'$5'/g' /tmp/server/$SERVER/terraform/main.tf
+cd /tmp/server/$SERVER/terraform/
+
+# Initialise the configuration
+sudo /var/lib/jenkins/bin/terraform init -input=false
+
+# Plan and deploy
+DO_TOKEN=$DO_API_KEY
+SSH_FINGERPRINT=$(cat /tmp/text)
+sudo /var/lib/jenkins/bin/terraform plan -input=false -var="do_token=${DO_TOKEN}" -var="pub_key=/tmp/server/$SERVER/$SERVER_PUBLIC_KEY" -var="pvt_key=/tmp/server/$SERVER/$SERVER" -var="ssh_fingerprint=${SSH_FINGERPRINT}" -out=droplet_create
+
+sudo /var/lib/jenkins/bin/terraform apply droplet_create
+
+
+cat /tmp/server/$SERVER/$SERVER
+# Terraform will run the above script to benchmark the server
+# Find your results in the Terraform output
+
+# Once finished, destroy the server
+
+#terraform plan -destroy -out=droplet_destroy
+#/var/lib/jenkins/bin/terraform apply droplet_destroy
+#sudo /var/lib/jenkins/bin/terraform destroy -input=false -var="do_token=${DO_TOKEN}" -var="pub_key=/tmp/server/test/test.pub" -var="pvt_key=/tmp/server/test/test" -var="ssh_fingerprint=${SSH_FINGERPRINT}"
+# Cleaning up
+#rm -rf /tmp/server/$SERVER/terraform/
